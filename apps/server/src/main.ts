065b87aa2a0275b1f6f667dc50139ecf22311774
William Sedlacek <wsedlacekc@gmail.com>
2020-08-31 23:14:54 -0700

    refactor(server): Remove boilerplate
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -11,10 +11,12 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(port).then(() => {
+    Logger.log(`Listening at http://localhost:${port}/${globalPrefix}`);
   });
 }
 
